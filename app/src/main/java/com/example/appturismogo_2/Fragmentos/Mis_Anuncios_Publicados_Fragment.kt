126d26f29c3fad258be959d18ed7a41afc5b5516
michael <17200780@ue.edu.pe>
2024-07-07 08:50:10 -0500

    Listar Anuncios propios y Anuncios Favoritos

--- app/src/main/java/com/example/appturismogo_2/Fragmentos/Mis_Anuncios_Publicados_Fragment.kt
+++ app/src/main/java/com/example/appturismogo_2/Fragmentos/Mis_Anuncios_Publicados_Fragment.kt
@@ -1,20 +1,76 @@
 package com.example.appturismogo_2.Fragmentos
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.appturismogo_2.Adaptadores.AdaptadorAnuncio
+import com.example.appturismogo_2.Modelo.ModeloAnuncio
 import com.example.appturismogo_2.R
+import com.example.appturismogo_2.databinding.FragmentMisAnunciosPublicadosBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Mis_Anuncios_Publicados_Fragment : Fragment() {
+
+    private lateinit var binding : FragmentMisAnunciosPublicadosBinding
+    private lateinit var mContext : Context
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var anunciosArrayList : ArrayList<ModeloAnuncio>
+    private lateinit var anunciosAdaptador : AdaptadorAnuncio
+
+    override fun onAttach(context: Context) {
+        this.mContext = context
+        super.onAttach(context)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis__anuncios__publicados_, container, false)
+        binding = FragmentMisAnunciosPublicadosBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        cargarMisAnuncios()
+    }
+
+    private fun cargarMisAnuncios() {
+        anunciosArrayList = ArrayList()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Anuncios")
+        ref.orderByChild("uid").equalTo(firebaseAuth.uid!!)
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    anunciosArrayList.clear()
+                    for (ds in snapshot.children){
+                        try{
+                            val modeloAnuncio= ds.getValue(ModeloAnuncio::class.java)
+                            anunciosArrayList.add(modeloAnuncio!!)
+                        }catch (e:Exception){
+
+                        }
+                    }
+
+                    anunciosAdaptador = AdaptadorAnuncio(mContext, anunciosArrayList)
+                    binding.misAnunciosRv.adapter = anunciosAdaptador
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
 
 }
